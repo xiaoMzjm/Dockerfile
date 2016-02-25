6ebdfc34fb37732afcadba725d4aba543427aeee
xiaoMzjm <383248689@qq.com>
2016-02-25 14:25:50 +0800

    修改脚本

--- nginx_keepalived/serverDown.sh
+++ nginx_keepalived/serverDown.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pkill keepalived
 /etc/init.d/networking restart
-echo "$1" | mutt -s "keepalived notify" heijueerror@163.com
+myip=`LC_ALL=C ifconfig|grep "inet addr:"|grep -v "127.0.0.1"|cut -d: -f2|awk '{print $1}'`
+echo "$myip  $1" | mutt -s "keepalived notify" heijueerror@163.com
--- nginx_keepalived/serverUp.sh
+++ nginx_keepalived/serverUp.sh
@@ -1,1 +1,3 @@
-echo "$1" | mutt -s "keepalived notify" heijueerror@163.com
+#!/usr/bin/env bash
+myip=`LC_ALL=C ifconfig|grep "inet addr:"|grep -v "127.0.0.1"|cut -d: -f2|awk '{print $1}'`
+echo "$myip $1" | mutt -s "keepalived notify" heijueerror@163.com
