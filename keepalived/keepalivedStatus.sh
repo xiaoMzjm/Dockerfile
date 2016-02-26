6a212d9ac0aa169e12640a7697a5de8134bc3b8a
xiaoMzjm <383248689@qq.com>
2016-02-26 11:57:44 +0800

    修改脚本，增加输出本机IP

--- keepalived/keepalivedStatus.sh
+++ keepalived/keepalivedStatus.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+myip=`LC_ALL=C ifconfig|grep "inet addr:"|grep -v "127.0.0.1"|cut -d: -f2|awk '{print $1}'`
 NOTIFYEMAIL="xxxxxxxxx@163.com"
 
-echo "$1 $2 $3" | mutt -s "keepalived notify" $NOTIFYEMAIL
+echo "$myip $1 $2 $3" | mutt -s "keepalived notify" $NOTIFYEMAIL
